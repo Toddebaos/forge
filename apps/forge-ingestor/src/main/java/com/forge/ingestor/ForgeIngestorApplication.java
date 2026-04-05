5a1b5fd67e3debfde6c93de722bea328963d65c8
Toddebaos <th3503an-s@student.lu.se>
2026-04-05 15:21:51 +0200

    feat: scaffold forge-ingestor Spring Boot project

---
+++ apps/forge-ingestor/src/main/java/com/forge/ingestor/ForgeIngestorApplication.java
@@ -0,0 +1,13 @@
+package com.forge.ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgeIngestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgeIngestorApplication.class, args);
+	}
+
+}
---
+++ apps/forge-ingestor/src/test/java/com/forge/ingestor/ForgeIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forge.ingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForgeIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
